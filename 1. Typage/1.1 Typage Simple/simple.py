700796808a0dc0a374b31b991a45658fa53e85a8
AliiiBenn <davidddpereiraaa6@gmail.com>
2024-03-04 15:53:57 +0100

    Update

---
+++ 1. Typage/1.1 Typage Simple/simple.py
@@ -0,0 +1,11 @@
+nombre_entier : int = 5
+nombre_flottant : float = 5.0
+
+booleen : bool = True
+booleen2 : bool = False
+
+booleen3: bool = 0  # Ne fonctionne pas
+booleen3: bool = bool(0) 
+
+
+chaine: str = "Bonjour"
